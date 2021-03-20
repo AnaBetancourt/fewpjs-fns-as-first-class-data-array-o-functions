922817b69fe0f033bdd8e80b0692afea715138fc
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-03-20 01:26:16 -0400

    commit tree 9e348089ab60be1242eaa77792b7e8bd26ca7300

---
+++ index.js
@@ -0,0 +1,48 @@
+const wakeDog = function(dogName, dogBreed){
+    const string = `Wake ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+const  leashDog = function(dogName, dogBreed){
+    const string = `Leash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+const walkToPark = function(dogName, dogBreed){
+    const string = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+const throwFrisbee = function(dogName, dogBreed){
+    const string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+const walkHome = function(dogName, dogBreed){
+    const string = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+ 
+const unleashDog = function(dogName, dogBreed){
+    const string = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(string);
+    return string;
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed){
+    let values = [];
+
+    for (const element of routine){
+        values.push(element(dogName, dogBreed));
+    };
+    
+    return values;
+}
+
